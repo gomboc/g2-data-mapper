a4228e5da11667303018ad14c09d297b78129b8f
Draško Gomboc <drasko.gomboc@gmail.com>
2012-02-25 14:50:51 +0100

    changes in zf app structure

--- sample/application/Bootstrap.php
+++ sample/application/Bootstrap.php
@@ -3,6 +3,57 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
+	
+	protected function _initAutoload()
+	{
+		// Rewriting mapper basepath
+		$resourceLoader = new Zend_Loader_Autoloader_Resource(array(
+        	'basePath'      => APPLICATION_PATH,
+          	'namespace'     => '',
+          	'resourceTypes' => array(
+				'mappers' => array(
+					'path'      => 'models/Mapper',
+					'namespace' => 'Model_Mapper',
+				),          
+		)));
+		
+		$autoloader = Zend_Loader_Autoloader::getInstance();
+		
+		//Module autoloader
+		$moduleAutoloader = new Zend_Application_Module_Autoloader(array(
+			'namespace' => '', 
+			'basePath' => APPLICATION_PATH
+		));
+		
+		$autoloader->pushAutoloader( $moduleAutoloader );
+	}
+	
 
+	protected function _initApp()
+	{
+	
+	}
+
+	
+	protected function _initDbAdapter()
+	{
+		try {
+			
+			$dbAdapter = $this->getPluginResource( 'db' )->getDbAdapter();
+		    $dbAdapter->getConnection();	    
+		    			
+		} catch ( Zend_Exception $e ) {		  
+			
+	     	throw new Exception( 'No database connection on your local mashine', 1101 );
+		}	
+	}
+	
+	
+	protected function _initDataMapper()
+	{
+		require_once 'G2/DataMapper.php';
+		
+		$dataMapper = new G2_DataMapper( $this->getPluginResource( 'db' )->getDbAdapter() );
+	}
 }
 
---
+++ sample/application/modules/default/controllers/IndexController.php
@@ -0,0 +1,38 @@
+<?php
+
+class IndexController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+       
+    }
+
+    public function indexAction()
+    {	
+		
+//		$collection = new G2_DataMapper_Collection();
+//		
+//		$domain = new G2_DataMapper_Domain();
+//		
+//		$domain->setId( 101 );
+//		
+//		echo $domain->getId();
+//		
+//		$identity = new G2_DataMapper_Identity();
+//		
+//		$identity->setXyz( 1 );
+//		
+//		$mapper = new G2_DataMapper_Mapper();
+//		
+//		$selection = new G2_DataMapper_Selection();
+//		
+//		$watcher = G2_DataMapper_Watcher::getInstance();
+//		
+//		$identityField = new G2_DataMapper_Identity_Field( 'name' );
+//		
+//		$factoryDomain = new G2_DataMapper_Factory_Domain();
+    }
+
+
+}
